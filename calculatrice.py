2597ea122bf392dff18a75a76b97b57a8c6a580e
Camille-Aimé Possamaï <camille-aime.possamai@wanadoo.fr>
2022-01-16 00:37:49 +0100

    boutons créés dans une boucle, affichés et sauvegardés dans un dict

--- calculatrice.py
+++ calculatrice.py
@@ -1,9 +1,10 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget, QGridLayout, QLineEdit, QPushButton, QVBoxLayout
+from PySide6.QtCore import Qt
 
 BUTTONS = { "C": (1, 0, 1, 1),
-            "E": (1, 1, 1, 1),
-            "=": (1, 2, 1, 1),
+            "\u25C0": (1, 1, 1, 1),
+            "=": (5, 3, 1, 1),
             "x": (1, 3, 1, 1),
             "7": (2, 0, 1, 1),
             "8": (2, 1, 1, 1),
@@ -17,7 +18,8 @@ BUTTONS = { "C": (1, 0, 1, 1),
             "3": (4, 2, 1, 1),
             "0": (5, 0, 1, 2),
             ".": (5, 2, 1, 1),
-            "+": (5, 3, 2, 1)
+            "+": (4, 3, 1, 1),
+            "/": (2, 3, 1, 1)
 }
 class Calculator(QWidget):
     def __init__(self):
@@ -29,57 +31,16 @@ class Calculator(QWidget):
         layout = QGridLayout(self)
         
         self.result_screen = QLineEdit()
-
+        self.result_screen.setText('0')
+        self.result_screen.setAlignment(Qt.AlignRight)
         layout.addWidget(self.result_screen, 0,0,1,4)
     
-        # for label, position in BUTTONS.items():
-        self.btn = QPushButton("C")
-        layout.addWidget(self.btn, 1,0,1,1)
-        self.btn = QPushButton("\u25C0")
-        layout.addWidget(self.btn, 1,1,1,1)
-        self.btn = QPushButton("x")
-        layout.addWidget(self.btn, 1,3,1,1)
-        self.btn = QPushButton("7")
-        layout.addWidget(self.btn, 2,0,1,1)
-        self.btn = QPushButton("4")
-        layout.addWidget(self.btn, 3,0,1,1)
-        self.btn = QPushButton("1")
-        layout.addWidget(self.btn, 4,0,1,1)
-        self.btn = QPushButton("0")
-        layout.addWidget(self.btn, 5,0,1,2)
-        self.btn = QPushButton("8")
-        layout.addWidget(self.btn, 2,1,1,1)
-        self.btn = QPushButton("6")
-        layout.addWidget(self.btn, 3,1,1,1)
-        self.btn = QPushButton("2")
-        layout.addWidget(self.btn, 4,1,1,1)
-        self.btn = QPushButton("9")
-        layout.addWidget(self.btn, 2,2,1,1)
-        self.btn = QPushButton("6")
-        layout.addWidget(self.btn, 3,2,1,1)
-        self.btn = QPushButton("3")
-        layout.addWidget(self.btn, 4,2,1,1)
-        self.btn = QPushButton(".")
-        layout.addWidget(self.btn, 5,2,1,1)
-        self.btn = QPushButton("/")
-        layout.addWidget(self.btn, 2,3,1,1)
-        self.btn = QPushButton("-")
-        layout.addWidget(self.btn, 3,3,1,1)
-
-        # plus_layout = QVBoxLayout()
-        # self.btn = QPushButton("+")
-        # self.btn.setFixedSize(45,68)
-        # plus_layout.addWidget(self.btn)
-        # layout.addLayout(plus_layout, 4, 3)
-
-
-        # self.btn = QPushButton("=")
-        # layout.addWidget(self.btn, 1,2,1,1)
-        self.btn = QPushButton("=")
-        layout.addWidget(self.btn, 5,3,1,1)
-        self.btn = QPushButton("+")
-        layout.addWidget(self.btn, 4,3, 1, 1)
-
+        self.dict_btn = {}
+        for label, position in BUTTONS.items():
+            btn = QPushButton(label)
+            layout.addWidget(btn, *position)
+            self.dict_btn[label] = btn
+        print(self.dict_btn['\u25C0'].text())
 
 app = QApplication(sys.argv)
 win = Calculator()
